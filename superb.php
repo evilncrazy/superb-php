625ae07f5cfc07bf975ac10bea35590e012ac418
evilncrazy <user2@server.fake>
2012-12-01 10:47:33 +1100

    Add helpers and shorthand tags

--- superb.php
+++ superb.php
@@ -7,6 +7,7 @@ class Superb {
    private $children = array();
    private $attrs = array();
    private $format;
+   private $last_return = true;
    
    public function __construct($name, $inner = null) {
       $this->attrs['%name'] = $name;
@@ -20,14 +21,15 @@ class Superb {
    }
    
    public function add($child) {
-      $this->children = array_merge($this->children, (array)$child);
+      if(is_array($child)) $this->children = array_merge($this->children, $child);
+      else $this->children[] = $child;
    }
    
    public function set($attr, $val) {
       $this->attrs[$attr] = $val;
    }
    
-   private function get($attr, $default = false) {
+   public function get($attr, $default = false) {
       return isset($this->attrs[$attr]) ? $this->attrs[$attr] : $default;
    }
    
@@ -69,7 +71,7 @@ class Superb {
                          ($this->get('%entity') ? 
                            htmlentities($child->as_string($new_indent)) : $child->as_string($new_indent));
             }
-            $inner .= ($this->get_format() == 'block' ? "\n" . $indent : '');
+            $inner .= ($this->get_format() == 'block' ? "\n" . $indent  : '');
          }
          
          foreach($this->attrs as $attr => $val) {
@@ -84,16 +86,38 @@ class Superb {
       }
    }
    
+   public function each($iterable, $iterator) {
+      /* check if iterable (does not work in all cases):
+         http://stackoverflow.com/questions/3584700/iterable-objects-and-array-type-hinting/ */
+      $this->last_return = false;
+      if((is_array($iterable) || is_a($iterable, 'Traversable')) && is_callable($iterator)) {
+         foreach($iterable as $item) {
+            if(call_user_func($iterator, ($sp = new Sp()), $item) !== false) {
+               $this->add($sp->get_top_su());
+               $this->last_return = true;
+            }
+         }
+      }
+      return $this;
+   }
+   
+   public function els($func) {
+      if($this->last_return == false && call_user_func($func, ($sp = new Sp())) !== false) {
+         $this->add($sp->get_top_su());
+         $this->last_return = true;
+      } else $this->last_return = false;
+      return $this;
+   }
+   
    public function __toString() {
-      return $this->as_string();
+      return $this->as_string() . "\n";
    }
 }
 
 class Sp {
    private static $aliases = array(
       'css' => array('%name' => 'link', 'rel' => 'stylesheet', 'type' => 'text/css', 'href' => null),
-      'js' => array('%name' => 'script', 'type' => 'text/javascript', 'src' => null),
-      'tag' => array('%name' => null)
+      'js' => array('%name' => 'script', 'type' => 'text/javascript', 'src' => null)
    );
    
    private $top_level_su = array();
@@ -104,8 +128,7 @@ class Sp {
    
    public static function parse_options($su, $options, $default = '') {
       foreach($options as $attr => $val) {
-         if(empty($val)) $val = $default;
-         $su->set($attr, $val);
+         $su->set($attr, empty($val) ? $default : $val);
       }
       return $su;
    }
@@ -116,42 +139,34 @@ class Sp {
          2. array: contains optional attributes (e.g. class, id, name)
          3. function: calls the function, with any markup added to this node */
       if(empty($su)) $su = new Superb($name); /* use existing Superb object? */
-      if(count($args)) {
-         $su_children = array();
-         foreach($args as $arg) {
-            if(is_a($arg, 'Superb')) {
-               $su_children[] = $arg;
-               
-               /* since $arg is now a child of this Superb object, 
-                  we must remove it from top_level_su as it is no longer 'top level' */
-               if(($key = array_search($arg, $this->top_level_su)) !== false) {
-                   unset($this->top_level_su[$key]);
-               }
-            } else if(is_a($arg, 'Sp')) {
-               $su_children = array_merge($su_children, $arg->get_top_su());
-            } else if(is_callable($arg)) {
-               /* given a function, so call it with a new Superb object as parameter */
-               call_user_func($arg, ($sp = new Sp()));
-               if($sp && is_a($sp, 'Sp')) {
-                  /* add any markup created by the Superb object passed to the function */
-                  $su_children = array_merge($su_children, $sp->get_top_su());
-               }
-            } else if(is_string($arg)) {
-               /* treat as a Sp::text tag */
-               if($name == 'text' || $name == 'comment') $su->set('%inner', $arg);
-               else $su_children[] = new Superb('text', $arg);
-            } else if(is_array($arg)) {
-               /* optional attributes */
-               $su = self::parse_options($su, $arg);
+      foreach($args as $arg) {
+         if(is_a($arg, 'Superb')) {
+            $su->add($arg);
+            
+            /* since $arg is now a child of this Superb object, 
+               we must remove it from top_level_su as it is no longer 'top level' */
+            if(($key = array_search($arg, $this->top_level_su)) !== false)
+                unset($this->top_level_su[$key]);
+         } else if(is_a($arg, 'Sp')) {
+            $su->add($arg->get_top_su());
+         } else if(is_callable($arg)) {
+            /* given a function, so call it with a new Superb object as parameter */
+            if(call_user_func($arg, ($sp = new Sp())) !== false) {
+               /* add any markup created by the Superb object passed to the function */
+               $su->add($sp->get_top_su());
             }
+         } else if(is_string($arg)) {
+            /* treat as a Sp::text tag */
+            if($name == 'text' || $name == 'comment') $su->set('%inner', $arg);
+            else $su->add(new Superb('text', $arg));
+         } else if(is_array($arg)) {
+            /* optional attributes */
+            $su = self::parse_options($su, $arg);
          }
-         
-         $su->add($su_children);
       }
 
       /* we record what markup has been generated by this particular Sp object */
       $this->top_level_su[] = $su;
-      
       return $su;
    }
    
@@ -159,8 +174,17 @@ class Sp {
    public function __call($name, $args) {
       if(isset(self::$aliases[$name]) && count($args)) {
          /* use the first arg as parameter of the alias, then parse the su object as normal */
-         return $this->parse_args(self::$aliases[$name]['%name'], array_slice($args, 1),
-            self::parse_options(new Superb(self::$aliases[$name]['%name']),  self::$aliases[$name], $args[0]));
+         return $this->parse_args('', array_slice($args, 1),
+            self::parse_options(new Superb(''),  self::$aliases[$name], $args[0]));
+      } else if($name == 't') {
+         /* need to parse: tag, tag #id, tag .class1 .class2, tag #id .class */
+         $chunks = explode(' ', $args[0]);
+         $su = new Superb($chunks[0]);
+         foreach($chunks as $chunk) {
+            if($chunk[0] == '.') $su->set('class', $su->get('class', '') . substr($chunk, 1));
+            else if($chunk[0] == '#') $su->set('id', substr($chunk, 1));
+         }
+         return $this->parse_args($chunks[0], array_slice($args, 1), $su);
       }
       
       return $this->parse_args($name, $args);
